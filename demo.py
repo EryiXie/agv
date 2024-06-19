ff2f2c6ee696b7429be7763d55460bf507549199
yaxu <yaxu0325@gmail.com>
2024-06-19 10:11:15 -0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,12 +13,6 @@ if __name__ == "__main__":
 			 
 			ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
-
-    while True:
-        
-    
-
-
 			sn30 = SN30_8BitDO()
     clock = pygame.time.Clock()
 
